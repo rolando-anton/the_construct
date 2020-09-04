a81dcf0fd928fbd3c265d0af69d2f3e26936a2f1
Rolando Anton <rolando@anton.sh>
2020-09-04 22:05:42 +0000

    random fixes

--- roles/bootstrap/files/change-to-static.ps1
+++ roles/bootstrap/files/change-to-static.ps1
@@ -1,18 +1,43 @@
-$MgmtIP=$args[0]
-$MgmtAdapters = gwmi -cl win32_networkadapterconfiguration -filter "ipenabled = 'true'" | ? {($_.ipaddress -like $MgmtIP) -and $_.dhcpEnabled -eq 'True' }
-$MgmtInterfaceIndex = $MgmtAdapters.InterfaceIndex
-Set-NetIPInterface -InterfaceIndex $MgmtInterfaceIndex -Dhcp Disabled
-New-NetIPAddress -InterfaceIndex "$MgmtInterfaceIndex" -AddressFamily IPv4 -PrefixLength 24 -IPAddress $MgmtIP
-$NonMgmtAdapters = gwmi -cl win32_networkadapterconfiguration -filter "ipenabled = 'true'" | ? {($_.ipaddress -notlike $MgmtIP) -and $_.dhcpEnabled -eq 'True' }
+# Author: Rolando Anton
+# Contact: rolando@anton.sh
+# Blog: https://rolando.anton.sh
 
-foreach ($NonMgmtAdapters in $NonMgmtAdapters) {
-$NonMgmtipAddress = $NonMgmtAdapters.IPAddress
-$NonMgmtsubnetMask = $NonMgmtAdapters.IPSubnet
-$NonMgmtdnsServers = $NonMgmtAdapters.DNSServerSearchOrder
-$NonMgmtdefaultGateway = $NonMgmtAdapters.DefaultIPGateway
-$NonMgmtAdapters.EnableStatic($NonMgmtipAddress,$NonMgmtsubnetMask)
-$NonMgmtAdapters.SetDNSServerSearchOrder($NonMgmtdnsServers)
-$NonMgmtAdapters.SetGateways($NonMgmtdefaultGateway)
+
+# Function from: https://d-fens.ch/2013/11/01/nobrainer-using-powershell-to-convert-an-ipv4-subnet-mask-length-into-a-subnet-mask-address/
+
+function Convert-IpAddressToMaskLength([string] $dottedIpAddressString)
+{
+  $result = 0; 
+  # ensure we have a valid IP address
+  [IPAddress] $ip = $dottedIpAddressString;
+  $octets = $ip.IPAddressToString.Split('.');
+  foreach($octet in $octets)
+  {
+    while(0 -ne $octet) 
+    {
+      $octet = ($octet -shl 1) -band [byte]::MaxValue
+      $result++; 
+    }
+  }
+  Write-Output $result;
 }
 
-Restart-Computer -Force
+$MgmtAdapters = Get-WmiObject win32_networkadapterconfiguration -filter "ipenabled='true'" | Where -Property DHCPEnabled -eq "True"
+
+foreach ($MgmtAdapters in $MgmtAdapters) {
+    $MgmtInterfaceIndex = $MgmtAdapters.InterfaceIndex
+    $MgmtipAddress = $MgmtAdapters.IPAddress
+    $MgmtsubnetMask = $MgmtAdapters.IPSubnet
+    $MgmtsubnetPrefix = Convert-IpAddressToMaskLength $MgmtsubnetMask
+    $MgmtdnsServers = $MgmtAdapters.DNSServerSearchOrder
+    $MgmtdefaultGateway = $MgmtAdapters.DefaultIPGateway
+    if ( ($MgmtdnsServers) -and ($MgmtdefaultGateway)) {
+       Set-NetIPInterface -InterfaceIndex $MgmtInterfaceIndex -Dhcp Disabled
+       New-NetIPAddress -InterfaceIndex "$MgmtInterfaceIndex" -AddressFamily IPv4 -PrefixLength "$MgmtsubnetPrefix" -IPAddress "$MgmtipAddress" -DefaultGateway "$MgmtdefaultGateway"
+       Set-DnsClientServerAddress -InterfaceIndex "$MgmtInterfaceIndex" -ServerAddresses  $MgmtdnsServers
+    }
+    if (!$MgmtdefaultGateway) {
+       Set-NetIPInterface -InterfaceIndex $MgmtInterfaceIndex -Dhcp Disabled
+       New-NetIPAddress -InterfaceIndex "$MgmtInterfaceIndex" -AddressFamily IPv4 -PrefixLength "$MgmtsubnetPrefix" -IPAddress "$MgmtipAddress"
+    }
+}
